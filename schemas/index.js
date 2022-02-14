b874c4ebeaffa86409a01f83d3daf16cd90e3ba8
minkimhere <mj.kim.tech@gmail.com>
2022-02-15 01:08:16 +0900

    몽고디비 주소 원래대로 변경

--- schemas/index.js
+++ schemas/index.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-    .connect("mongodb://localhost:27017/admin", {
-      dbName:'devel-rope',
-    })
+    .connect("mongodb://localhost:27017/devel-rope")
     .catch(err => console.log(err));
 };
 
