d7e96fdd3e2288b7add77699b1ce439e5b4ff141
minkimhere <mj.kim.tech@gmail.com>
2022-02-16 00:43:09 +0900

    post관련 정보 불러오기 comment_cnt 추가

--- routes/post.js
+++ routes/post.js
@@ -2,14 +2,25 @@ const express = require("express");
 const router = express.Router(); //라우터 선언
 const Post = require("../schemas/post");
 const authMiddleware = require("../middlewares/auth-middleware");
+const Comment = require("../schemas/comment");
 
 // // Post 관련 정보 불러오기
 router.get("/post", async (req, res) => {
   try {
     const post = await Post.find({}).sort("-date").exec();
     const user = res.locals.users;
+
+    const postId = post.postId;
+    const comment = await Comment.find({ postId });
+    const comment_cnt = comment.length;
+    await Post.updateOne(
+      { postId: postId },
+      { $set: { comment_cnt: comment_cnt } }
+    );
+
     res.json({
-      post, user
+      post,
+      user,
     });
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -18,29 +29,37 @@ router.get("/post", async (req, res) => {
   }
 });
 
-
-
 //Post 생성
 router.post("/post", authMiddleware, async (req, res) => {
-  const {content, imgUrl } = req.body;
-  const userIcon = res.locals.users.userIcon
-  const nickname = res.locals.users.nickname
-  const userId = res.locals.users.userId
+  const { content, imgUrl } = req.body;
+  const userIcon = res.locals.users.userIcon;
+  const nickname = res.locals.users.nickname;
+  const userId = res.locals.users.userId;
 
   //db의 date 호출전 날짜 형식 맞추기   //2022-02-03 09:40:10 형식으로 출력
-const date = new Date(+new Date() + 3240 * 10000).toISOString().replace("T", " ").replace(/\..*/, "");
+  const date = new Date(+new Date() + 3240 * 10000)
+    .toISOString()
+    .replace("T", " ")
+    .replace(/\..*/, "");
   try {
-    const result = await Post.create({nickname, userIcon, content, imgUrl, date, userId});
+    const result = await Post.create({
+      nickname,
+      userIcon,
+      content,
+      imgUrl,
+      date,
+      userId,
+    });
     const postId = result.postId;
     res.status(200).json({
       postId,
       ok: true,
-      message: "생성 성공"
+      message: "생성 성공",
     });
   } catch (err) {
     res.status(400).json({
       ok: false,
-      errorMessage: "생성 실패"
+      errorMessage: "생성 실패",
     });
   }
 });
@@ -52,24 +71,24 @@ const date = new Date(+new Date() + 3240 * 10000).toISOString().replace("T", " "
 //     "imgUrl": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimage.msscdn.net%2Fimages%2Fgoods_img%2F20191216%2F1252014%2F1252014_1_500.jpg&imgrefurl=https%3A%2F%2Fstore.musinsa.com%2Fapp%2Fgoods%2F1252014&tbnid=3vNacgxz1vVfBM&vet=12ahUKEwjxhYDzu_f1AhVsz4sBHeXSA2QQMygeegUIARCUAg..i&docid=5VYXw3FcqVcrnM&w=500&h=500&q=%EA%B0%95%EC%95%84%EC%A7%80&ved=2ahUKEwjxhYDzu_f1AhVsz4sBHeXSA2QQMygeegUIARCUAg",
 // }
 // 썬더 Auth > OAuth2 >Access Token  (master토큰)
-//   
+//
 
-// Post 상세 보기 
+// Post 상세 보기
 router.get("/detail/:postId", async (req, res) => {
-    try {
-      const { postId } = req.params;
-      console.log( typeof postId);
-      // const [detail] = await Post.findOne({ postId });
-      console.log( postId);
-      console.log("111넘버postId", Number(postId)  );
-      const detail = await Post.findOne({ postId: Number(postId) });
-      const userIcon = detail.userIcon // 로컬에서 userIcon 찾기 
-      
-      res.status(200).json({
-        ok: true,
+  try {
+    const { postId } = req.params;
+    console.log(typeof postId);
+    // const [detail] = await Post.findOne({ postId });
+    console.log(postId);
+    console.log("111넘버postId", Number(postId));
+    const detail = await Post.findOne({ postId: Number(postId) });
+    const userIcon = detail.userIcon; // 로컬에서 userIcon 찾기
+
+    res.status(200).json({
+      ok: true,
       detail,
       userIcon,
-      message: "상세페이지 보기 성공"
+      message: "상세페이지 보기 성공",
     });
   } catch (err) {
     res.status(400).json({
@@ -80,12 +99,11 @@ router.get("/detail/:postId", async (req, res) => {
   }
 });
 
-
 // //Post 수정하기
 router.put("/item/:postId", authMiddleware, async (req, res) => {
   const { postId } = req.params;
   const { content, imgUrl } = req.body;
-  const nickname = res.locals.users.nickname
+  const nickname = res.locals.users.nickname;
   const existPost = await Post.findOne({ postId: Number(postId) });
   console.log(11, nickname);
   console.log(22, existPost);
@@ -93,45 +111,43 @@ router.put("/item/:postId", authMiddleware, async (req, res) => {
     return res.status(400).json({
       existPost,
       ok: false,
-      message: "수정 실패"
+      message: "수정 실패",
     });
   } else if (existPost.nickname === nickname) {
-        await Post.updateOne({ postId: Number(postId) }, { $set: { content, imgUrl }});
-    }
-    res.status(200).json({
-        ok: true,
-        errorMessage: "수정 성공",
-      });
-      
+    await Post.updateOne(
+      { postId: Number(postId) },
+      { $set: { content, imgUrl } }
+    );
+  }
+  res.status(200).json({
+    ok: true,
+    errorMessage: "수정 성공",
+  });
 });
 // {
 //   "content": "수정수정테스트",
 //   "imgUrl": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fpurplejam.kr%2Fcontent%2Fimages%2F2020%2F02%2Fshutterstock_1544993138-1.jpg&imgrefurl=https%3A%2F%2Fpurplejam.kr%2Fcat-whiskers%2F&tbnid=PPqYOsFYQ7knIM&vet=12ahUKEwj21oXQvfn1AhXZOnAKHcShDaYQMyhMegQIARBu..i&docid=zt5_8DNuqu8prM&w=2000&h=1333&q=%EA%B3%A0%EC%96%91%EC%9D%B4&ved=2ahUKEwj21oXQvfn1AhXZOnAKHcShDaYQMyhMegQIARBu"
 // }
 
-
-
 // //Post 삭제하기
-router.delete("/item/:postId", authMiddleware, async  (req, res) => {
+router.delete("/item/:postId", authMiddleware, async (req, res) => {
   const { postId } = req.params;
-  const nickname = res.locals.users.nickname
+  const nickname = res.locals.users.nickname;
   const existPost = await Post.findOne({ postId: Number(postId) });
   if (existPost.nickname !== nickname) {
     return res.status(400).json({
       nickname,
       existPost,
       ok: false,
-      message: "삭제 실패"
+      message: "삭제 실패",
     });
   } else if (existPost.nickname === nickname) {
-      await Post.deleteOne({ postId: Number(postId)});
+    await Post.deleteOne({ postId: Number(postId) });
   }
   res.status(200).json({
     ok: true,
-    errorMessage: "삭제 성공"
+    errorMessage: "삭제 성공",
   });
 });
-
-
 
 module.exports = router;
