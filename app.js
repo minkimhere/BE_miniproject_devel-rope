e6dc23a25839a3fd8d7d1597c3c1efba0872a940
suzy <2021234@naver.com>
2022-02-17 22:11:27 +0900

    220217-5-주석수정

--- app.js
+++ app.js
@@ -2,27 +2,11 @@ const express = require('express');
 const cors = require("cors");
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT || 3000; // process.env.port||3000의 뜻은 process.env라른 객체에 port라는 설정이 있다면 그 속성을 사용하고 없다면 3000을 사용한다는 뜻이다.
 
-//cors모듈 사용.
-// app.use(cors());
-// origin: Front End ip or url
-// app.use(cors({ origin: "http://localhost:3000" }));
-// app.use("*", cors());
+// process.env.port||3000의 뜻은 process.env라른 객체에 port라는 설정이 있다면 
+// 그 속성을 사용하고 없다면 3000을 사용한다는 뜻이다.
+const port = process.env.PORT || 3000; 
 
-// origin: "http://localhost:3000",
-
-// const corsOption = {
-//   origin: "http://devel-rope.site/",
-//   credentials: true,
-// };
-
-// const corsOption = {
-//   origin: "http://devel-rope.site.s3-website.ap-northeast-2.amazonaws.com/",
-//   credentials: true,
-// };
-
-// app.use(cors(corsOption));
 app.use(cors());
 
 //스키마 연결
@@ -36,29 +20,20 @@ const requestMiddleware = (req, res, next) => {
   next();
 };
 
-//바디로 들어온 제이슨 형태를 파싱
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(requestMiddleware);
-
 
 //router 가져오기
 const postRouter = require("./routes/post"); 
 const commentRouter = require("./routes/comment"); 
 const userRouter = require("./routes/user"); 
 
+//바디로 들어온 제이슨 형태를 파싱
 //Router 미들웨어 사용
-app.use("/api", [postRouter]); 
-app.use("/api", [commentRouter]); 
-app.use("/api", [userRouter]);    // "/"라는 경로로 들어왔을때 userRouter실행
-
-//uploads폴더안 파일을 url로 접근
-//사용자가 upload파일 접근  url에 /uploads/123.png (사용자파일이름)
-app.use('/uploads', express.static("uploads")); 
-// 라우팅 정의
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/static/upload.html");
-});
+app.use('/api', express.urlencoded({ extended: false }), [ 
+  userRouter, 
+  postRouter, 
+  commentRouter, ]);
 
 app.listen(port, () => {
   console.log(port, ': 포트로 서버가 열렸어요!');
